e77001db2185761500bc02b492cb783ac5f6d651
asakon <kondoasako@gmail.com>
2020-04-26 03:27:30 +0900

    balloon

---
+++ src/components/atoms/Balloon/index.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Balloon from './index.js';
+
+// export default stories => stories
+//    .add('デフォルト', () => <Balloon />);
+
+export default { title: 'Balloon' };
+
+export const withText = () => <span class="balloon">削除する</span>;
