f8df3c3eeb95efbcd814278d4d6a41fc43c02490
asakon <kondoasako@gmail.com>
2020-04-26 06:00:29 +0900

    テキストを消してみた

--- src/components/atoms/Balloon/index.js
+++ src/components/atoms/Balloon/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './styles.css';
 
-const Balloon = () => <span claasName={ styles.ballon  }>削除する</span>;
+const Balloon = () => <span claasName={ styles.ballon  }></span>;
 
 export default Balloon;
